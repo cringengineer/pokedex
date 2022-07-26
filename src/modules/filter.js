2da4160f01000e36effa835a778816eb72637064
cringengineer <cringengineer@gmail.com>
2022-07-26 17:44:40 +0300

    fix: fix filter

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -1,58 +1,40 @@
 const typesBtn = document.querySelectorAll('.type__item');
-const closeBtn = document.querySelector('.close__filter');
-const filterPopup = document.querySelector('#filter');
-const openFilter = document.querySelector('.filter__button');
-const applyBtn = document.querySelector('.apply');
-const reset = document.querySelector('.reset');
+const btnFilter = document.querySelector('.filter__button');
+const typeList = document.querySelector('.type__list')
 
 
 
-openFilter.addEventListener('click', function() {
-    filterPopup.classList.remove('hiden');
-    filterPopup.classList.add('filter__popup');
+btnFilter.addEventListener('click', function() {
+    if (btnFilter.classList.contains('opened')) {
+        btnFilter.classList.remove('opened');
+        typeList.style.display = '';
+    } else {
+        btnFilter.classList.add('opened');
+        typeList.style.display = 'grid';
+    }
 })
 
-closeBtn.addEventListener('click',function() {
-    filterPopup.classList.remove('filter__popup');
-    filterPopup.classList.add('hiden');
-})
-
-let choosedPokemons = [];
 
 typesBtn.forEach(function(e) {
     e.addEventListener('click',function() {
         if(this.classList.contains('choosed__type')) {
             this.classList.remove('choosed__type');
-            let indexOfPokemon = choosedPokemons.indexOf(this.value.toLowerCase());
-            choosedPokemons.splice(indexOfPokemon, 1);
+            cards.forEach(function(e) {
+                    e.classList.remove('hiden')
+            })
         } else {
+            typesBtn.forEach(function(e) {
+                e.classList.remove('choosed__type')
+            })
             this.classList.add('choosed__type');
-            choosedPokemons.push(this.value.toLowerCase());
-            console.log(choosedPokemons)
+            let currentType = this.value.toLowerCase();
+            console.log(currentType)
+            cards.forEach(function(e) {
+                if(!e.classList.contains(currentType)) {
+                    e.classList.add('hiden')
+                }
+            })
+            
         }
     })
-})
-
-
-applyBtn.addEventListener('click', function() {
-    console.log(choosedPokemons)
-    cards.forEach(function(card) {
-        for(let i = 0;i<choosedPokemons.length;i++) {
-            if(!card.classList.contains(choosedPokemons[i])) {
-                card.classList.add('hiden')
-            }
-        }
-    })
-})
-
-reset.addEventListener('click',function() {
-    cards.forEach(function(card) {
-        if(card.classList.contains('hiden')) {
-            card.classList.remove('hiden');
-        }
-    })
-    typesBtn.forEach(function(e) {
-        e.classList.remove('choosed__type')
-    })
-    choosedPokemons = []
 })
